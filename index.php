03fa7196f32f2e477487040a7f7a78bb54154e27
Emanuel Ferreira <emnarlanferreira@gmail.com>
2022-08-09 21:49:59 -0300

    Adição do campo Tipo (tela de cadastro)

--- index.php
+++ index.php
@@ -12,13 +12,14 @@ $method = $_SERVER['REQUEST_METHOD'];
 switch ($method) {
     case 'POST':
         $user = json_decode( file_get_contents('php://input') );
-        $sql = 'INSERT INTO usuarios(nome, email, senha, dtAdicao) VALUES(:nome, :email, :senha, :dtAdicao)';
+        $sql = 'INSERT INTO usuarios(dt_cadastro, nome, email, senha, tipo) VALUES(:dt_cadastro, :nome, :email, :senha, :tipo)';
         $stmt = $conn->prepare($sql);
         $dataCreated = date('d-m-Y');
+        $stmt->bindParam(':dt_cadastro', $user->dt_cadastro);
         $stmt->bindParam(':nome', $user->nome);
         $stmt->bindParam(':email', $user->email);
         $stmt->bindParam(':senha', $user->senha);
-        $stmt->bindParam(':dtAdicao', $user->dtAdicao);
+        $stmt->bindParam(':tipo', $user->tipo);
         if ($stmt->execute()) {
             $response = ['status' => 1, 'message' => 'Funcionou :D'];
         } else {
